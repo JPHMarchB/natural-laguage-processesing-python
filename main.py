8f6e44947dce41f28a9dfc1d0515a9edf8113537
Joseph Marchbanks <153705879+JPHMarchB@users.noreply.github.com>
2024-04-30 12:51:03 -0600

    Completed sentiment analyzer for textblob, allowed for additional sentiment analysis using NLTK

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from textblob import TextBlob
 from newspaper import Article
+from nltk.sentiment import SentimentIntensityAnalyzer
 
 
 URL ='https://en.wikipedia.org/wiki/ISDN'
@@ -10,5 +11,16 @@ article.download()
 article.parse()
 article.nlp()
 
-TEXT = article.text
-print(TEXT)
+TEXT = article.summary
+# print(TEXT)
+
+# Using Vader/NLTK
+SIA = SentimentIntensityAnalyzer()
+sentiment_scores = SIA.polarity_scores(TEXT)
+
+# Using TextBlob
+blob = TextBlob(TEXT)
+sentiment = blob.sentiment.polarity # -1 to 1
+
+print(sentiment)
+print("Sentiment Scores:", sentiment_scores)
